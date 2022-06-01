ca71c2e218bef1a30ee52a6729e8cfa5d0c131dd
ebola <627702281@qq.com>
2022-06-01 16:59:36 +0800

    Rewrite quaprog with colvector

--- lib/quadric_programming.h
+++ lib/quadric_programming.h
@@ -20,14 +20,14 @@ Matrix quaprog_equ(const Matrix &H, const Matrix &c, const Matrix &A, const Matr
     return B.T()*b-G*c;
 }
 
-Matrix quaprog_subproblem(const Matrix &H, const Matrix &c, const Matrix &Ae, const Matrix &be, Matrix &lambda){
+ColVector quaprog_subproblem(const Matrix &H, const ColVector &c, const Matrix &Ae, const ColVector &be, ColVector &lambda){
     Matrix ginvH = pinv(H);
     if(Ae.n){
-        Matrix rb = Ae*ginvH*c+be;
+        ColVector rb = Ae*ginvH*c+be;
         lambda = pinv(Ae*ginvH*Ae.T())*rb;
         return ginvH*(Ae.T()*lambda-c);
     } else {
-        lambda = zeros(be.n,1);
+        lambda = ColVector(be.n);
         return -ginvH*c;
     }
 }
@@ -46,26 +46,31 @@ int checkednum(const bool *p, int l, int r){
  * 另有参数x0为指定的初始可行点
  * 调用方法：sol=quaprog(H,c,Ae,be,Ai,bi,x0,[MAXN])
  * ****************************************************************/
-Matrix quaprog(const Matrix &H, const Matrix &c, const Matrix &Ae, const Matrix &be, 
-               const Matrix &Ai, const Matrix &bi, const Matrix &x0, const int MAXN=1000, const double eps=1e-10){
-    Matrix x = x0;
+Matrix quaprog(const Matrix &H, const ColVector &c, const Matrix &Ae, const ColVector &be, 
+               const Matrix &Ai, const ColVector &bi, const ColVector &x0, const int MAXN=1000, const double eps=1e-10){
+    ColVector x = x0;
     int n = x.n, ne = be.n, ni = bi.n, step = 0;
     bool *idx = new bool[ni];
     for(int i = 0; i < ni; i++)
-        idx[i] = !( value(Ai.getSubmatrix(i,i,0,n-1)*x) > bi[i][0]+eps );
+        idx[i] = !( Ai.getRow(i)*x > bi[i]+eps );
     for(; step <= MAXN; step++){
         Matrix Aee;
         if(ne) Aee = Ae;
         for(int j = 0; j < ni; j++)
-            if(idx[j]) Aee = mergeRow(Aee,Ai.getSubmatrix(j,j,0,n-1));
-        Matrix gk = H*x+c, lamk, dk;
+            if(idx[j]) Aee = mergeRow(Aee,Ai.getRow(j));
+        ColVector gk = H*x+c, lamk, dk;
         int m = Aee.n;
-        dk = quaprog_subproblem(H,gk,Aee,zeros(m,1),lamk);
+        dk = quaprog_subproblem(H,gk,Aee,zeroCol(m),lamk);
+#ifdef DEBUG
+        std::cerr << "x = " << x.T() << std::endl;
+        std::cerr << "dk = " << dk.T() << std::endl;
+        std::cerr << "lamk = " << lamk.T() << std::endl << std::endl;
+#endif
         if(dk.vecnorm(2) <= eps){
             double y = 0.0; int jk;
             if(lamk.n > ne)
                 for(int j = ne; j < lamk.n; j++)
-                    if(lamk[j][0]<y) y=lamk[j][0], jk=j;
+                    if(lamk[j]<y) y=lamk[j], jk=j;
             if(y >= 0) break;
             for(int i = 0; i < ni; i++)
                 if(idx[i] && ne+checkednum(idx,0,i)==jk+1){
@@ -76,14 +81,14 @@ Matrix quaprog(const Matrix &H, const Matrix &c, const Matrix &Ae, const Matrix 
             int ti;
             double alpha = 1.0, tm = 1.0;
             for(int i = 0; i < ni; i++){
-                Matrix tmp = Ai.getSubmatrix(i,i,0,n-1);
-                if( !idx[i] && value(tmp*dk)<0 ){
-                    double tm1 = (bi[i][0]-value(tmp*x))/value(tmp*dk);
+                RowVector tmp = Ai.getRow(i);
+                if( !idx[i] && tmp*dk<0 ){
+                    double tm1 = (bi[i]-tmp*x)/(tmp*dk);
                     if(tm1 < tm) tm = tm1, ti = i;
                 }
             }
             alpha = std::min(alpha, tm);
-            x = x+alpha*dk;
+            x = x + alpha*dk;
             if(tm < 1) idx[ti] = true;
         }
     }
@@ -92,7 +97,7 @@ Matrix quaprog(const Matrix &H, const Matrix &c, const Matrix &Ae, const Matrix 
     if(step<=MAXN) std::cerr << "Finished Succesfully. Total Steps: " << step << std::endl;
     else std::cerr << "Terminated. Too many steps." << std::endl;
     std::cerr << "Optimal Point: " << x.T() << std::endl;
-    std::cerr << "OPtimal Value: " << 0.5*value(x.T()*H*x)+value(c.T()*x) << std::endl << std::endl;
+    std::cerr << "OPtimal Value: " << 0.5*(x.T()*H*x) + c.T()*x << std::endl << std::endl;
 #endif
     return x;
 }
