e7b20b549a8d8b9aaf214d4ca14c95ed56398a09
ebola <627702281@qq.com>
2022-06-09 22:55:00 +0800

    oh gush

--- 3_Conjugate_Gradient_example.cpp
+++ 3_Conjugate_Gradient_example.cpp
@@ -13,12 +13,12 @@ int main(){
 
     cout << "Restart FR-CG Method solve n-dim rosenbrock minimal" << endl;
     rosenbrock::n = n;
-    Matrix x2 = CG_FR(rosenbrock::f, rosenbrock::grad, rosenbrock::initial(), 1e-6, 0.1, 0.4);
+    ColVector x2 = CG_FR(rosenbrock::f, rosenbrock::grad, rosenbrock::initial(), 1e-6, 0.1, 0.4);
     cout << "min f = f(" << x2.T() << ") = " << rosenbrock::f(x2) << endl << endl;
 
     cout << "Restart FR-CG Method (gradfree) solve n-dim rosenbrock minimal" << endl;
     rosenbrock::n = n;
-    Matrix x3 = CG_FR_gradfree(rosenbrock::f, rosenbrock::initial(), 1e-6, 0.1, 0.4);
+    ColVector x3 = CG_FR_gradfree(rosenbrock::f, rosenbrock::initial(), 1e-6, 0.1, 0.4);
     cout << "min f = f(" << x3.T() << ") = " << rosenbrock::f(x3) << endl << endl;
     return 0;
 }
